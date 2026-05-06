539d199accdf134cd496a2fb74e2f6504dd4bdc0
Naninf <thaysafernandavictor@gmail.com>
2026-05-06 14:56:08 -0300

    Aula 10
    Começando pelas novas libs:Flask_WTF e email_validator. Flask_WTF é a lib de formulários onde permite cria-los.
    
    Segundo pelo forms.py que eu preenchi ele para criar os formulários, com duas classes que recebem FlaskForms como parãmetro, a classe FormLogin é a que faz e valida completamente o login do usuário levando email, com os validadores: DataRequired, que faz o campo obrigatoriamente ter algum dado, e email, onde verifica se é um email valido. Senha que leva o mesmo DataRequired. E o butao_confirmacao onde só leva o Datarequired.
    E a classe FormCriarConta, que contem os campos: email, username, senha, onde leva o validador length que obriga a senha ter de 6 a 20 caracteres, e confirmacao_senha, onde a única diferença do campo de senha é levar a validação de EqualTo que obriga a ser igual ao campo mencionado no caso o campo senha, e no fim o butao_confirmacao. Mas com um adendo na classe onde tem uma função que busca no banco de dados se já tem o email, e interrompe a criação caso tenha, tudo por causa que na criação do banco de dados coloquei um parametro no email unique=True.

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,1 +1,22 @@
 # criar os formulários
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, length, ValidationError
+from fakepinterest.models import Usuario
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    butao_confirmacao = SubmitField("Fazer cogin")
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Nome de usuário", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), length(6,20)])
+    confirmacao_senha = PasswordField("Confirmar senha", validators=[DataRequired(), EqualTo("senha")])
+    butao_confirmacao = SubmitField("Criar conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("E-mail já cadastrado, faça login para continuar")
