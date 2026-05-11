7e0b2afdb3a90d155da991ec95bd35c0b8516396
Naninf <thaysafernandavictor@gmail.com>
2026-05-11 16:43:51 -0300

    Aula 11
    Login inteiro do site, mas não está funcionando ainda, só visual

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -2,10 +2,18 @@
 from flask import render_template, url_for
 from fakepinterest import app
 from flask_login import login_required
+from fakepinterest.forms import FormLogin, FormCriarConta
 
-@app.route('/') #Criando rota para o site
+@app.route('/', methods=['GET', 'POST']) #Criando rota para o site
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form=formlogin)
+
+
+@app.route("/criar-conta")
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form=formcriarconta)
 
 @app.route('/perfil/<nome>')
 @login_required
